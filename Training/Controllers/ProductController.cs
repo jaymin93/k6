2ba796cb7b876d35344068a11f007b50beeaf9a7
jaymin suthar <jayminsuthar93@outlook.com>
2021-12-28 16:19:22 +0530

    Add project files.

---
+++ Training/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Training.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        [Route("AddProduct")]
+        public void AddProduct(Product product)
+        {
+            Product.AddProduct(_context, product);
+        }
+        [HttpPost]
+        [Route("RemoveProduct")]
+        public void RemoveProduct(int id)
+        {
+            Product.RemoveProductById(_context, id);
+        }
+        [HttpGet]
+        [Route("GetAllProducts")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return Product.GetAllProduct(_context);
+        }
+        [HttpGet]
+        [Route("GetProduct")]
+        public Product GetProduct(int id)
+        {
+            return Product.GetProductById(_context, id);
+        }
+    }
+}
---
+++ Training/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Training
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public static void AddProduct(ApplicationDbContext context, Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+        }
+        public static Product GetProductById(ApplicationDbContext context, int id)
+        {
+            return context.Products.FirstOrDefault((p) => p.Id == id);
+        }
+        public static List<Product> GetAllProduct(ApplicationDbContext context)
+        {
+            return context.Products.ToList();
+        }
+        public static void RemoveProductById(ApplicationDbContext context, int id)
+        {
+            var productToRemove = context.Products.FirstOrDefault((p) => p.Id == id);
+            context.Products.Remove(productToRemove);
+            context.SaveChanges();
+        }
+    }
+}
