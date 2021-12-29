335b1c1425419536c52643aa01da53365d655491
jaymin suthar <jayminsuthar93@outlook.com>
2021-12-29 10:22:13 +0530

    updated k6 script

--- Training/script.js
+++ Training/script.js
@@ -1,26 +1,33 @@
 ﻿import http from 'k6/http';
-import { check } from 'k6';
-import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
-export const options = {
-    stages: [
-        { duration: '10s', target: 10 },
-        { duration: '20s' },
-        { duration: '10s', target: 5 },
-    ],
-    thresholds: {
-        http_req_duration: ['p(95)<250'],
-    },
-};
+import { sleep } from 'k6';
+
 export default function () {
-    let res = http.get(`${__ENV.API_PROTOCOL}://${__ENV.API_BASEURL}/Product/GetAllProducts`);
-    check(res, {
-        'is status 200': (r) => r.status === 200,
-    });
+    const res = http.get('https://test.k6.io');
+    sleep(1);
 }
-export function handleSummary(data) {
-    let filepath = `./${__ENV.TESTRESULT_FILENAME}-result.xml`;
-    return {
-        'stdout': textSummary(data, { indent: ' ', enableColors: true }),
-        './loadtest-results.xml': jUnit(data),
-    }
-}
+////import http from 'k6/http';
+////import { check } from 'k6';
+////import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+////export const options = {
+////    stages: [
+////        { duration: '10s', target: 10 },
+////        { duration: '20s' },
+////        { duration: '10s', target: 5 },
+////    ],
+////    thresholds: {
+////        http_req_duration: ['p(95)<250'],
+////    },
+////};
+////export default function () {
+////    let res = http.get(`${__ENV.API_PROTOCOL}://${__ENV.API_BASEURL}/Product/GetAllProducts`);
+////    check(res, {
+////        'is status 200': (r) => r.status === 200,
+////    });
+////}
+////export function handleSummary(data) {
+////    let filepath = `./${__ENV.TESTRESULT_FILENAME}-result.xml`;
+////    return {
+////        'stdout': textSummary(data, { indent: ' ', enableColors: true }),
+////        './loadtest-results.xml': jUnit(data),
+////    }
+////}
