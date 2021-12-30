1794e74c5e4c4fc591753d1459714c2f8b3d6bd7
jaymin93 <jayminsuthar93@outlook.com>
2021-12-30 12:13:41 +0530

    Update script.js

--- Training/script.js
+++ Training/script.js
@@ -1,57 +1,15 @@
 import http from 'k6/http';
-import { check } from 'k6';
-import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import { sleep } from 'k6';
+
 export const options = {
   stages: [
-    { duration: '10s' }
+    { duration: '10s', target: 15 },
+    { duration: '20s', target: 15 },
+    { duration: '10s', target: 0 },
   ],
-  thresholds: {
-    http_req_duration: ['p(95)<250'],
-  },
 };
-export default function () {
-  let res = http.get(`https://k6ldtst.azurewebsites.net/Product/GetAllProducts`);
-  check(res, {
-    'is status 200': (r) => r.status === 200,
-  });
-}
-export function handleSummary(data) {
-  let filepath = `./result.xml`;
-    return {
-        'stdout': textSummary(data, { indent: ' ', enableColors: true}), 
-        './results.xml': jUnit(data), 
-    }
-}
-////import http from 'k6/http';
-////import { sleep } from 'k6';
 
-////export default function () {
-////    const res = http.get('https://test.k6.io');
-////    sleep(1);
-////}
-////import http from 'k6/http';
-////import { check } from 'k6';
-////import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
-////export const options = {
-////    stages: [
-////        { duration: '10s', target: 10 },
-////        { duration: '20s' },
-////        { duration: '10s', target: 5 },
-////    ],
-////    thresholds: {
-////        http_req_duration: ['p(95)<250'],
-////    },
-////};
-////export default function () {
-////    let res = http.get(`${__ENV.API_PROTOCOL}://${__ENV.API_BASEURL}/Product/GetAllProducts`);
-////    check(res, {
-////        'is status 200': (r) => r.status === 200,
-////    });
-////}
-////export function handleSummary(data) {
-////    let filepath = `./${__ENV.TESTRESULT_FILENAME}-result.xml`;
-////    return {
-////        'stdout': textSummary(data, { indent: ' ', enableColors: true }),
-////        './loadtest-results.xml': jUnit(data),
-////    }
-////}
+export default function () {
+  const res = http.get('https://test.k6.io');
+  sleep(1);
+}
