931b5c66c3d15176b9104840032dc6abc02285c9
jaymin93 <jayminsuthar93@outlook.com>
2022-01-04 12:11:22 +0530

    Update script.js

--- Training/script.js
+++ Training/script.js
@@ -1,19 +1,28 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check } from 'k6';
+import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export const options = {
-  stages: [
-    { duration: '10s', target: 15 },
-    { duration: '20s', target: 15 },
-    { duration: '10s', target: 0 },
-  ],
-  thresholds: {
-    http_req_duration: ['p(95)<250'],
-  },
-};
+    stages: [
+      { duration: '10s', target: 15 },
+      { duration: '20s', target: 15 },
+      { duration: '10s', target: 0 },
+    ],
+    thresholds: {
+      http_req_duration: ['p(95)<250'],
+    },
+  };
 
 export default function () {
-  const res = http.get('https://k6httploadtesr.azurewebsites.net/api/Function1?code=f6F06agGFO7yp2K1GEkOjD4ZbR27QayHv2Acopy/b6g9VDha9NPJ6w==');
-  sleep(1);
+  let res = http.get('https://k6httploadtesr.azurewebsites.net/api/Function1?code=f6F06agGFO7yp2K1GEkOjD4ZbR27QayHv2Acopy/b6g9VDha9NPJ6w==');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
 }
-
+export function handleSummary(data) {
+  let filepath = `./result.xml`;
+    return {
+        'stdout': textSummary(data, { indent: ' ', enableColors: true}), 
+        './results.xml': jUnit(data), 
+    }
+}
