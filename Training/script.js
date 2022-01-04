c5bf21fd14303c8b5929ffad2e7989ddfafac52d
jaymin93 <jayminsuthar93@outlook.com>
2022-01-04 10:45:58 +0530

    Update script.js

--- Training/script.js
+++ Training/script.js
@@ -3,13 +3,17 @@ import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '50s', target: 15 },
-    { duration: '60s', target: 15 },
-    { duration: '100s', target: 30 },
+    { duration: '10s', target: 15 },
+    { duration: '20s', target: 15 },
+    { duration: '10s', target: 0 },
   ],
+  thresholds: {
+    http_req_duration: ['p(95)<250'],
+  },
 };
 
 export default function () {
   const res = http.get('https://k6httploadtesr.azurewebsites.net/api/Function1?code=f6F06agGFO7yp2K1GEkOjD4ZbR27QayHv2Acopy/b6g9VDha9NPJ6w==');
   sleep(1);
 }
+
